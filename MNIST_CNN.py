2adece33788ba9764c6a0c0f82c4480a24fcb159
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-18 22:25:31 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -4,7 +4,7 @@ Created on Mon Apr  1 20:38:27 2019
 
 @author: vyom.agrwl
 """
-'''Trains a simple convnet on the MNIST dataset. atas
+'''Trains a simple convnet on the MNIST dataset.
 '''
 
 from __future__ import print_function
