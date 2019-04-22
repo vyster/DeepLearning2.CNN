e476fcee7f93680ff1f58af15836ba61b815d246
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-22 17:28:48 +0530

    Update AlexnetArchiVy.py

--- AlexnetArchiVy.py
+++ AlexnetArchiVy.py
@@ -11,3 +11,7 @@ classifier = Sequential()
 
 classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'relu'))
 
+
+
+classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'relu'))
+
