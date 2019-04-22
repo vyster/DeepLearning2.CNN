7ef899d06292040413e2eea6be0972b41cab6237
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-22 23:24:37 +0530

    Update AlexnetArchiVy.py

--- AlexnetArchiVy.py
+++ AlexnetArchiVy.py
@@ -6,9 +6,7 @@ from keras.layers import Flatten
 from keras.layers import Dense
 from keras.layers import Dropout
 
-classifier = Sequential()
-
-
+classifier = Sequential
 classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'relu'))
 
 
