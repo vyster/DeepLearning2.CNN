a664c624f300325a8123155c16d2e15c6f108838
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-22 23:25:32 +0530

    Update AlexnetArchiVy.py

--- AlexnetArchiVy.py
+++ AlexnetArchiVy.py
@@ -10,5 +10,9 @@ classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_
 
 
 
+
+classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'relu'))
+
+
 classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'relu'))
 
