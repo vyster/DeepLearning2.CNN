7f60664d0431f9ebd63fc47a561f4a98b2588920
Vyom Agarwal <42782904+vyster@users.noreply.github.com>
2019-04-25 05:53:41 +0530

    Update AlexnetArchiVy.py

--- AlexnetArchiVy.py
+++ AlexnetArchiVy.py
@@ -5,4 +5,3 @@ from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Dropout
 
-classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(227, 227, 3), activation = 'Sigmoid' and Bro)
